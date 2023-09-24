9e8ce9614193f84a5efbc17a3a8702413ea7fb9a
AnirbanDesktop <abhattacharje@socivolta.com>
2023-09-24 11:40:41 -0400

    merchant is part of transaction

--- paymenthook/models/definitions.py
+++ paymenthook/models/definitions.py
@@ -3,8 +3,6 @@ import datetime
 import enum 
 from typing import List
 
-
-
 prefab_fields = {
     "transaction_id":Field(validation_alias="Psp Reference"),
     "merchant_id":Field(validation_alias="Merchant Account"),  
@@ -22,8 +20,7 @@ class PayoutSplitType(enum.Enum) :
     FEE="FEE"
   
 class BaseTransactionInfo(BaseModel) :
-    transaction_id:str=prefab_fields["transaction_id"]
-    merchant_id:str=prefab_fields["merchant_id"]
+    transaction_id:str=prefab_fields["transaction_id"]   
     amount:float=0.0
     timestamp:datetime.datetime=prefab_fields["timestamp"]
     
@@ -31,7 +28,8 @@ class PayOutSegment(BaseTransactionInfo) :
     split_id:PayoutSplitType
 
 
-class TransactionNotification(BaseTransactionInfo):   
+class TransactionNotification(BaseTransactionInfo):  
+    merchant_id:str=prefab_fields["merchant_id"] 
     transaction_type:TrasnactionType
     seller_split:PayOutSegment=None
     valpay_split:PayOutSegment=None
